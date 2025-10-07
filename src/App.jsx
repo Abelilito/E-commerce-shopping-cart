a707aa2474a777adc1a6c3ce6deb691ac278ef62
Abel Ilito Limoma <ilitoabel@gmail.com>
2025-10-07 14:24:37 +0200

    Feat: add legal notice

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import ShoppingCard from "./components/ShoppingCard";
 import { Panier } from "./components/Panier/Panier";
+import LegalNotice from "./components/LegalNotice/LegalNotice";
 
 function App() {
   const [panier, setPanier] = useState([]);
 
   return (
-    <div className="flex justify-around flex-col lg:flex-row items-center lg:items-start">
-      <ShoppingCard panier={panier} setPanier={setPanier} />
-      <Panier panier={panier} />
-    </div>
+    <>
+      <div className="flex justify-around flex-col lg:flex-row items-center lg:items-start mt-0 lg:mt-[7rem]">
+        <ShoppingCard panier={panier} setPanier={setPanier} />
+        <Panier panier={panier} />
+      </div>
+      <div className="pt-[5rem]">
+        <LegalNotice />
+      </div>
+    </>
   );
 }
 
